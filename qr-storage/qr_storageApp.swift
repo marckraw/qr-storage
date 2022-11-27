c566cb60200d4ccdb00266e1bcf5fd1503dece26
marckraw <marckraw@icloud.com>
2022-11-27 10:57:14 +0100

    Initial Commit

---
+++ qr-storage/qr_storageApp.swift
@@ -0,0 +1,17 @@
+//
+//  qr_storageApp.swift
+//  qr-storage
+//
+//  Created by marckraw on 27.11.22.
+//
+
+import SwiftUI
+
+@main
+struct qr_storageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
