335e02b1a98ac8fa513be6116f76d563a5c05e5b
marckraw <marckraw@icloud.com>
2022-11-27 12:20:33 +0100

    Exploration

--- qr-storage/ContentView.swift
+++ qr-storage/ContentView.swift
@@ -10,11 +10,45 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Text("Whatever!")
-            Text("Another text")
-            Button("Button") {
-                print("Hey you clkicked me")
-            }
+            Greeting()
+            Greeting()
+            Greeting()
+            Greeting()
+            Greeting()
+        }
+//        ScrollView {
+//            VStack {
+//                Text("Whatever!")
+//                Text("Another text")
+//                Button("Button") {
+//                    print("Hey you clkicked me")
+//                }
+//                ForEach(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { item in
+//                    Text("HSTack text")
+//                        .font(.subheadline)
+//                        .fontWeight(.bold)
+//                        .foregroundColor(.purple)
+//                        .padding(12)
+//                        .background(Color.yellow)
+//                        .padding(24)
+//                        .background(Color.green)
+//                        .padding(24)
+//                        .background(Color.orange)
+//                        .padding(12)
+//                        .contrast(2.0)
+//                }
+//            }
+//        }
+    }
+}
+
+struct Greeting: View {
+    @State var name = ""
+    
+    var body: some View {
+        VStack {
+            TextField("Enter your name", text: $name)
+            Text("Hello \(name)")
         }
     }
 }
